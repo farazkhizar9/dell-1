d5d29192cf1ad842c4ce9ab24ea9b04cfd5495c2
FARAZ KHIZAR <faraz.khizar.ai@gmail.com>
2025-09-18 16:46:46 +0500

    first

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,10 +4,17 @@ import React from "react";
 const About: React.FC = () => {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-white">
-      <h1 className="text-3xl font-semibold mb-4">About Us</h1>
-      <p className="max-w-lg text-center text-gray-600">
-        This is the About page. Here you can write details about yourself or
-        your project.
+      <h1 className="text-3xl font-semibold mb-4 text-blue-500">About Us</h1>
+      <p className="max-w-lg text-center text-blue-600">
+        My name is Muhammad Faraz Khizar. My father’s name is Khizar Hayat.
+         I am currently 19 years oldI have a strong interest in education, and I enjoy exploring new ideas and learning about technology.
+I chose the field of Artificial Intelligence (AI) because I believe the future belongs to AI.
+ This technology is rapidly transforming every industry—especially medicine and information technology (IT).
+  My goal is to bring AI into the medical world to help save lives and improve healthcare systems.
+Today, AI has already revolutionized fields like Web Development, and many other areas of IT.
+ I want to be a part of this technological evolution and contribute to its growth.
+I also believe in innovation and creativity. With the right vision and consistent effort,
+ I am confident that technology can help solve some of the world’s biggest challenges.
       </p>
     </main>
   );
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,8 +4,11 @@ import React from "react";
 const Contact: React.FC = () => {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
-      <h1 className="text-3xl font-semibold mb-4">Contact Us</h1>
-      <p className="text-gray-600">You can reach us at: example@email.com</p>
+      <h1 className="text-3xl font-semibold mb-4 text-blue-500">Contact Us</h1>
+      <p className="text-blue-600 font-bold">gmail:farazkhizar45@gmail.com
+        phone number:0345-6789012
+        address:123 Main St, City, Country
+      </p>
     </main>
   );
 };
